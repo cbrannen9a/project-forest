c07d720d213dc404cdbb234bb29acd9fa5770269
Chris Brannen <cbrannen@outlook.com>
2024-08-29 10:03:04 +0100

    feat: add env variable, control urls and randomise ordering

--- apps/foreman/src/constants.ts
+++ apps/foreman/src/constants.ts
@@ -1,5 +1,6 @@
-export const iterations = 100;
+export const defaultIterations = 1000;
 
+export const controlUrls = ["http://localhost:3000/control/a"];
 export const urls = [
   "http://localhost:3000/example-1/a",
   "http://localhost:3000/example-1/b",
--- apps/foreman/src/index.ts
+++ apps/foreman/src/index.ts
@@ -1,26 +1,36 @@
 import puppeteer from "puppeteer";
-import { iterations, urls } from "./constants";
+import { controlUrls, defaultIterations, urls } from "./constants";
 
 async function navigateToUrls(urls: string[]) {
-  const browser = await puppeteer.launch();
+  try {
+    const browser = await puppeteer.launch();
 
-  for (const url of urls) {
-    const page = await browser.newPage();
-    await page.goto(url);
-    // Wait
-    await new Promise((resolve) => {
-      setTimeout(resolve, 2000);
-    });
-    await page.close();
+    for (const url of urls) {
+      const page = await browser.newPage();
+      await page.setCacheEnabled(false); // disable cache
+      await page.goto(url);
+      // Wait
+      await new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+      });
+      await page.close();
+    }
+
+    await browser.close();
+  } catch (error) {
+    console.error(error);
   }
-
-  await browser.close();
 }
 
 async function main() {
-  for (let i = 0; i < iterations; i++) {
+  for (
+    let i = 0;
+    i < parseInt(process.env.ITERATIONS || `${defaultIterations}`);
+    i++
+  ) {
     console.log(`Iteration ${i + 1}`);
-    await navigateToUrls(urls);
+    const randomisedUrls = urls.sort(() => Math.random() - 0.5);
+    await navigateToUrls([...controlUrls, ...randomisedUrls]);
   }
 }
 
